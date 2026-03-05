e126141aee82de1b32048532aa302434f4571b94
Lup1n5 <146459441+Lup1n5@users.noreply.github.com>
2026-03-05 00:26:17 +0000

    eaea

--- E/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ E/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -3,12 +3,13 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
-
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class ShooterSubsystem extends SubsystemBase {
     // Define your shooter motors and any necessary variables here
-    final TalonFX m_LoaderMotor = new TalonFX(ShooterConstants.kShooterLoaderMotorCanID);
+    final SparkMax m_LoaderMotor = new SparkMax(ShooterConstants.kShooterLoaderMotorCanID, MotorType.kBrushless);
     final TalonFX m_RearMotor = new TalonFX(ShooterConstants.kShooterRearMotorCanID);
     final TalonFX m_FrontUpperMotor = new TalonFX(ShooterConstants.kShooterFrontUpperMotorCanID);
     final TalonFX m_FrontLowerMotor = new TalonFX(ShooterConstants.kShooterFrontLowerMotorCanID);
